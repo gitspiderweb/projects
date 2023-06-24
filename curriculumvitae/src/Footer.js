c78ae71dcd3806b10b4876e73fdc0b9970d1e955
gitspiderweb <cabatuanmarkjay.it@gmail.com>
2023-06-24 09:14:56 +0800

    cv - update github link

--- curriculumvitae/src/Footer.js
+++ curriculumvitae/src/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className='Footer'>
             <ul>
                 <li><a href="https://www.linkedin.com/in/mark-jay-cabatuan-1014111a3/">Linkedin</a></li>
-                <li><a href="https://github.com/markedJ">GitHub</a></li>
+                <li><a href="https://github.com/gitspiderweb">GitHub</a></li>
                 <li><a href="mailto:cabatuanmarkjay.it@gmail.com">Email</a></li>
                 <li><p>Call: +63 907 012 2304</p></li>
                 <li><p>Copyright &copy; {today.getFullYear()}</p></li>
